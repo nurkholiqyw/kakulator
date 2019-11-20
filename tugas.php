ece55c59576c049b7e450380f7dcd43007c82561
Nur Kholiq Yoga W <nur.kholiqyw@gmail.com>
2019-11-20 13:53:16 +0700

    Membuat Kakulator

---
+++ tugas.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Kalkulator</title>
+	<!-- 39.28 -->
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	
+	<div class="kalkulator">
+		<h2 class="judul">KALKULATOR</h2>
+		
+		<form method="post" action="tugas1.php">			
+			<input type="text" name="bil1" class="bil" autocomplete="off" placeholder="">
+			<input type="text" name="bil2" class="bil" autocomplete="off" placeholder="">
+			<select class="opt" name="operasi">
+				<option value="tambah">+</option>
+				<option value="kurang">-</option>
+				<option value="kali">x</option>
+				<option value="bagi">/</option>
+			</select>
+			<button type="submit" name="hitung" class="tombol">Hitung</button>
+
+
+		
+
+
+			
+	</div>
+</body>
+</html>
---
+++ tugas1.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HASIL</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<?php 
+	if(isset($_POST['hitung'])){
+		$bil1 = $_POST['bil1'];
+		$bil2 = $_POST['bil2'];
+		$operasi = $_POST['operasi'];
+		switch ($operasi) {
+			case 'tambah':
+				$hasil = $bil1+$bil2;
+			break;
+			case 'kurang':
+				$hasil = $bil1-$bil2;
+			break;
+			case 'kali':
+				$hasil = $bil1*$bil2;
+			break;
+			case 'bagi':
+				$hasil = $bil1/$bil2;
+			break;			
+		}
+	}
+	?>
+
+<div class="kalkulator">
+	<ul>
+		<font color="White">Hasilnya Adalah : </font>
+		</form>
+		<?php if(isset($_POST['hitung'])){ ?>
+			<input type="text" value="<?php echo $hasil; ?>" class="bil">
+		<?php }else{ ?>
+			<input type="text" value="0" class="bil">
+		<?php } ?>	
+	</ul>
+</div>
+	
+	
+</body>
+</html>
